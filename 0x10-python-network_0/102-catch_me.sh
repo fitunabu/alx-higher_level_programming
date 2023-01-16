80d370aec445953deb759db6f7c176b728e08062
fitunabu <106749942+fitunabu@users.noreply.github.com>
2023-01-16 18:19:18 +0300

    catch-me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Solution to task 9
-curl -sL -X PUT -H 'Origin:You got me!' -d 'user_id=98' 0.0.0.0:5000/catch_me
+# causes server to respond with a certain msg
+curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" "0.0.0.0:5000/catch_me"
