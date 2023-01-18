aaf38fb32d830f12277a8f3405c39955b40aea0f
fitunabu <106749942+fitunabu@users.noreply.github.com>
2023-01-18 09:57:36 +0300

    urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# Python script that fetches https://intranet.hbtn.io/status
-import urllib.request
+""" Fetches https://intranet.hbtn.io/status with urlib"""
+from urllib import request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
+if __name__ == '__main__':
+    req = request.Request("https://intranet.hbtn.io/status")
+    with request.urlopen(req) as response:
+        r = response.read()
+        s = "Body response:\n\t- type: {}\n\t- content: {}"
+        s += "\n\t- utf8 content: {}"
+        print(s.format(type(r), r, r.decode('utf-8')))
